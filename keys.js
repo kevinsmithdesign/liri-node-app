b8fe402679de5c71589dc4d0cc3fb80da16028ba
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2017-01-11 21:24:07 -0600

    adding initial files for liri command line app

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+  consumer_key: '<n7gAnV2gZQIjD7sY1cug9cCON>',
+  consumer_secret: '<Ei3i5eycVytJQIXX3grh7lNvraa3uetrgfKP6p2G0v2HDcDRMg>',
+  access_token_key: '<817592968366485505-OYCmAIYVZSQLVJgrCFuFpaLEC7XBsOj>', /* if doesn't work, change back to '<input here>', directions do not specify. */
+  access_token_secret: '<XfO8t7VuGEMh1ZbgIa7nPl5H4WKff2VbCtfqVZAud3Odc>', /* if doesn't work, change back to '<input here>', directions do not specify. */
+}

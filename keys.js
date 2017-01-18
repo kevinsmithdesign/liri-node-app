c2ceb0816ae3ba91f2c79abbb834cf718f3dc306
Kevin Smith <mrkevinsmithdesign@yahoo.com>
2017-01-17 21:15:03 -0600

    Delete keys.js

--- keys.js
+++
@@ -1,8 +0,0 @@
-console.log('this is loaded');
-
-exports.twitterKeys = {
-  consumer_key: 'n7gAnV2gZQIjD7sY1cug9cCON',
-  consumer_secret: 'Ei3i5eycVytJQIXX3grh7lNvraa3uetrgfKP6p2G0v2HDcDRMg',
-  access_token_key: '817592968366485505-OYCmAIYVZSQLVJgrCFuFpaLEC7XBsOj', 
-  access_token_secret: 'XfO8t7VuGEMh1ZbgIa7nPl5H4WKff2VbCtfqVZAud3Odc', 
-}
